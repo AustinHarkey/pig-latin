ed32bbf79b3e52548aa94b2ddd234542adc413ab
Austin Harkey <austindharkey@gmail.com>
2019-03-19 20:45:03 -0700

    updates

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let input = document.querySelector('.input');
 let submit = document.querySelector('.submit');
 let output = document.querySelector('.output');
+let vowels = ['a', 'e', 'i', 'o', 'u'];
 
 submit.addEventListener('click', function() {
-    let splitInput = input.value.split("");
-    let reverseInput = splitInput.reverse("");
-    let finalOutput = reverseInput.join("");
-    output.innerHTML = finalOutput;
+    let outputString;
+    
+    if(firstLetter == 'a' || firstLetter == 'e' || firstLetter == 'i' || firstLetter == 'o' || firstLetter == 'u') {
+        outputString = input.value + '-ay';
+        console.log(outputString);
+    } else {
+        let inputSplit = input.value.split("");
+        let firstLetter = inputSplit.shift();
+    }
+    
+
+    // console.log(inputSplit);
+    // console.log(firstLetter);
+    
 });
